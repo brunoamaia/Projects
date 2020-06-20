cbc889efbc2ec72d6d7bfcf54a53f0517abc4b75
brunoamaia <brunoamaia94@gmail.com>
2020-06-19 23:00:36 -0300

    utilização do switch para os numeros

--- calculator/funcoes.js
+++ calculator/funcoes.js
@@ -283,7 +283,7 @@ function point() {
     
     
 }
-function one() {
+/*function one() {
     txt += 1
     display.innerHTML = `${txt}`
 }
@@ -322,4 +322,50 @@ function nine() {
 function zero() {
     txt += 0
     display.innerHTML = `${txt}`
+} */
+
+function numb(cond) {
+    let val = Number(cond)
+    switch (val) {
+        case 0:
+            txt += 0
+            display.innerHTML = `${txt}`
+            break;
+        case 1:
+            txt += 1
+            display.innerHTML = `${txt}`
+            break;
+        case 2:
+            txt += 2
+            display.innerHTML = `${txt}`
+            break;
+        case 3:
+            txt += 3
+            display.innerHTML = `${txt}`
+            break;
+        case 4:
+            txt += 4
+            display.innerHTML = `${txt}`
+            break;
+        case 5:
+            txt += 5
+            display.innerHTML = `${txt}`
+            break;
+        case 6:
+            txt += 6
+            display.innerHTML = `${txt}`
+            break;
+        case 7:
+            txt += 7
+            display.innerHTML = `${txt}`
+            break;
+        case 8:
+            txt += 8
+            display.innerHTML = `${txt}`
+            break;
+        case 9:
+            txt += 9
+            display.innerHTML = `${txt}`
+            break;
+    }
 }
