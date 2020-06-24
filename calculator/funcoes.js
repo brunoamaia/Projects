64ced827623f2e7421cb31e82218b06ca274c98b
brunoamaia <brunoamaia94@gmail.com>
2020-06-24 11:53:22 -0300

    calculo da porcentagem

--- calculator/funcoes.js
+++ calculator/funcoes.js
@@ -153,20 +153,53 @@ function calculator() {         // Calculate the result
 
     if (len > 1){             //Verificar se tem elementos suficientes para rezlizar alguma operação
         validador = 1
+    } else if (oper[0] == '% ') {
+        validador = 1
     }
 
     while (validador == 1) {
+        let p_per = oper.indexOf('% ')
+        while (p_per != -1) {
+            info.innerHTML += `<br><br>Porcentagem`
+            if (p_per > 0) {
+                if (oper[p_per-1] == ' + ' ) {
+                    counts[p_per-1] = counts[p_per-1]*(1+counts[p_per]/100)
+                    oper.splice(p_per, 1)     // Remove as operações realizadas
+                    oper.splice(p_per - 1, 1)
+                    counts.splice(p_per, 1)
+                    info.innerHTML += 'soma'
+                } else if (oper[p_per-1] == ' - ' ) {
+                    counts[p_per-1] = counts[p_per-1]*(1-counts[p_per]/100)
+                    oper.splice(p_per, 1)     // Remove as operações realizadas
+                    oper.splice(p_per - 1, 1)
+                    counts.splice(p_per, 1)
+                    info.innerHTML += 'subtração'
+                } else {
+                    counts[p_per] = Number(counts[p_per]/100)
+                    oper.splice(p_per, 1)     // RRemove as operações realizadas
+                }
+            } else {
+                counts[p_per] = Number(counts[p_per]/100)
+                oper.splice(p_per, 1)     // RRemove as operações realizadas
+            }
+            p_per = oper.indexOf('% ')
+            info.innerHTML += `<br> Val = ${counts} <br> Ope = ${oper} ` 
+        }
+
         let p_mult = oper.indexOf(' * ')       // realiza a multiplicação
         while (p_mult != -1) {
+            info.innerHTML += `<br><br>Multiplicação`
             counts[p_mult] = Number(counts[p_mult]) * Number(counts[p_mult+1])  
             counts.splice(p_mult+1, 1)      // Remove o segundo elemento da multiplicação
             oper.splice(p_mult, 1)     // Remove a multiplicação realizada
 
             p_mult = oper.indexOf(' * ')
+            info.innerHTML += `<br> Val = ${counts} <br> Ope = ${oper} ` 
         }
         
         let p_div = oper.indexOf(' / ')       // realiza a divisão
         while (p_div != -1) {
+            info.innerHTML += `<br><br>Divisão`
             if (counts[p_div+1] == 0) {
                 result = 'Not divide by 0!'
                 return notnumber = 3
@@ -176,33 +209,40 @@ function calculator() {         // Calculate the result
                 oper.splice(p_div, 1)
             }
             p_div = oper.indexOf(' / ')
+            info.innerHTML += `<br> Val = ${counts} <br> Ope = ${oper} ` 
         }
 
         let p_sum = oper.indexOf(' + ')     // Realiza a Soma
         while (p_sum != -1) {
+            info.innerHTML += `<br><br>Soma`
             if (p_sum > 0 ) {   // Verificar se o primeiro valor é negativo (o Segundo não precisa)
                 if (oper[p_sum-1] == ' - ') {   // Se o 1º for neg, inverte seu sinal
                     let res = Number((counts[p_sum])*(-1) + counts[p_sum+1])
+                    info.innerHTML += `<br> Valor = ${res}` 
                     if (res < 0 ) { // se o resultado for negativo, altera o sinal no vetor de sinais
+                        info.innerHTML += '<br> Negativo'
                         counts[p_sum] = res*(-1)
                         oper[p_sum-1] = ' - '
                     } else {
+                        info.innerHTML += '<br> Positivo'
                         counts[p_sum] = res
                         oper[p_sum-1] = ' + '
                     }
                 }
             } else {
-                counts[p_sum] = Number(counts[p_sum] - counts[p_sum+1])  
+                counts[p_sum] = Number(counts[p_sum] +   counts[p_sum+1])  
             }
             counts.splice(p_sum+1, 1)
             oper.splice(p_sum, 1)
 
             p_sum = oper.indexOf(' + ')
+            info.innerHTML += `<br> Val = ${counts} <br> Ope = ${oper} ` 
         }
 
 
         let p_sub = oper.indexOf(' - ')     // Realiza a subtração
         while (p_sub != -1) {
+            info.innerHTML += `<br><br>Subtração`
             if (p_sub > 0) {    // Verificar se o primeiro valor é negativo (o Segundo não precisa)
                 if (oper[p_sub-1] == ' - ') {   // Se o 1º for neg, inverte seu sinal
                     let res = Number((counts[p_sub])*(-1) - counts[p_sub+1])
@@ -221,6 +261,7 @@ function calculator() {         // Calculate the result
             oper.splice(p_sub, 1)
             
             p_sub = oper.indexOf(' - ')
+            info.innerHTML += `<br> Val = ${counts} <br> Ope = ${oper} ` 
         }
         
         n = counts.length                   // Teste para finalização das operações 
@@ -303,6 +344,7 @@ function signal() {
 }
 
 function clearall() {
+    info.innerHTML=''
     display.innerHTML = 'Reseted!'
     histor.innerHTML = '..........'
     txt = ''
