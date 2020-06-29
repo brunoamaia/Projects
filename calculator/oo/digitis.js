acf8ee05349aff529f53e7fd71f45d62c68673cf
brunoamaia <brunoamaia94@gmail.com>
2020-06-29 09:15:05 -0300

    reset digit

--- calculator/oo/digitis.js
+++ calculator/oo/digitis.js
@@ -1,52 +1,8 @@
 // Insert the value of the number pressioned
 // 
 
-function digit (numb, inf) {
-    console.log(`\nvalor = ${numb} txt = ${inf}`)
-    let val = num.toString(numb)
-    let txt = inf
-    switch (val) {
-        case 0:
-            val += 0
-            console.log(`val = ${val}`);
-            return txt = val
-        case 1:
-            val += 1
-            console.log(`val = ${val}`);
-            return txt = val
-        case 2:
-            dis += 2
-            display.innerHTML = `${txt}`
-            return txt = dis
-        case 3:
-            dis += 3
-            display.innerHTML = `${txt}`
-            return txt = dis
-        case 4:
-            dis += 4
-            display.innerHTML = `${txt}`
-            return txt = dis
-        case 5:
-            dis += 5
-            display.innerHTML = `${txt}`
-            return txt = dis
-        case 6:
-            dis += 6
-            display.innerHTML = `${txt}`
-            return txt = dis
-        case 7:
-            dis += 7
-            display.innerHTML = `${txt}`
-            return txt = dis
-        case 8:
-            dis += 8
-            display.innerHTML = `${txt}`
-            return txt = dis
-        case 9:
-            dis += 9
-            display.innerHTML = `${txt}`
-            return txt = dis
-    }
+class digit {
+    
 }
 
 export { digit }
